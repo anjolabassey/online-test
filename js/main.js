0689aae5fadc5879c7082fbbdf19bc2e5142f351
Anjola <anjolabassey@yahoo.com>
2018-05-03 21:07:39 +0100

    added functionality for next button

--- js/main.js
+++ js/main.js
@@ -1,28 +1,45 @@
-var name = document.getElementById("name").value;
-var matNo = document.getElementById("matno").value;
-var subject = document.getElementById("subjects");
-
-
+var sub = document.getElementById("register");
+var subject;
 var main = document.getElementById("test");
-var obj;
-//var correct = document.getElementById("one").value;
-//var one = document.getElementById("one").value;
-//var two = document.getElementById("two").value;
-//var three = document.getElementById("three").value;
+var controls = document.getElementById("buttons");
+var counter = 0;
+var score;
+var questions;
 
-//var options = [ one, two, three];
-var score; 
-var picked;
 
+sub.addEventListener("click", function(event) {
+	event.preventDefault();
+	subject = document.getElementById("subjects").value;
+	//validateForm();
+	storeUser();
+	clearDiv();
+	getTest(subject);
+	//createNext();
+	// displayNext();
+});
+
+//validates the form: user cant register empty fields
+function validateForm() {
+	var n = document.forms["user"]["name"].value;
+	var m = document.forms["user"]["matno"].value;
+	
+    if (n == "" || m == "") {
+		alert("Please fill in your information");
+		return false;  
+	} 
+}
 
 // stores the users information to local(session) storage
 function storeUser() {
+	var name = document.getElementById("name").value;
+	var matNo = document.getElementById("matno").value;
+	
+
 	if (typeof(Storage) !== "undefined") {
 		// Code for localStorage/sessionStorage.
 		sessionStorage.setItem("name", name);
 		sessionStorage.setItem("matno", matNo);
-		sessionStorage.setItem("subject", subject.value);
-		
+		sessionStorage.setItem("subject", subject);	
 	} else {
 		// Sorry! No Web Storage support..
 		alert("Your browser does not have Web Storage support");
@@ -30,118 +47,66 @@ function storeUser() {
 }
 
 // clears the page(after user info is stored)
-function clearPage() {
-	document.getElementById("buttons").innerHTML= "";
-	document.getElementById("test").innerHTML = "";
-	
+function clearDiv() {
+	document.getElementById("test").innerHTML = "";		
 }
 
-//adds event listener for the register button(events to take palce after user clicks)
-document.getElementById("register").addEventListener("click", function() {
-	storeUser();
-	// clearPage();
-	// changeQuestion();
-	
-});
-
-// creates the test page
-function createTest() {
-	var question = document.createElement("h3");
-	var node = document.createTextNode(obj.subjects.biology.q1.question);
-	question.appendChild(node);
-
-	// var next = document.createElement("input");
-	// next.setAttribute('type', "submit");
-	// next.setAttribute('value', "Next");
-
-	// var opts = document.createElement("input");
-	// opts.setAttribute('type', "radio");
-	// opts.setAttribute('name', "options");
-	
-
-
-	// var opt1 = document.createElement("label");
-	// opt1.innerHTML = obj.subjects.biology.q1.options[0];
-	// opts.appendChild(opt1);
-	
-	var nav = document.getElementById("buttons");
-	nav.appendChild(next);
-	main.appendChild(question);
-	// main.appendChild(opts);
-	
-	
-
-}
-
-// changes the question 
-function changeQuestion() {
+function getData(result) { 
 	var xhttp = new XMLHttpRequest();
 	xhttp.overrideMimeType("application/json");
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			obj = JSON.parse(this.response);
-			//getData();
-			console.log(obj.subjects);
-	        console.log(subject.value);
-		}
+			result(xhttp.responseText);
+			}
 		
 	};
 	xhttp.open("GET", "test.json", true);
 	xhttp.send();
 }
 
-function getData() {
-	
-	var a = ""; var b = ""; var c = ""; 
-	
+function getTest(subject) {
+	var a = ""; var b = ""; var c = ""; var q = "";
+	getData(function (obj) {
+		result = JSON.parse(obj);
+		questions = result.subjects[subject];
+				// for (i=0; i < result.subjects[subject].length; i++) {
+					
+				// }
+			document.getElementById("title").innerHTML = subject + " Test";
 
-	for (i in obj) {
-		 
-		a += "<input type='radio' id='one'>" + obj.subjects.biology.q1.options[0] + "</input>" + "<br>";
-		b += "<input type='radio' id='two'>" + obj.subjects.biology.q1.options[1] + "</input>" + "<br>";
-		c += "<input type='radio' id='three'>" + obj.subjects.biology.q1.options[2] + "</input>";
+			q += "<h3 id='quest'>" + questions[counter].question + "</h3>";	
+			a += "<input type='radio' id='one'>" + questions[counter].options[0] + "</input>" + "<br>";
+			b += "<input type='radio' id='two'>" + questions[counter].options[1] + "</input>" + "<br>";
+			c += "<input type='radio' id='three'>" + questions[counter].options[2] + "</input>";
+						
+			main.innerHTML =  q + a + b + c;
+				
+						
+	});
+	createNext();
+}
 
-		
-		main.innerHTML =  a + b + c;
-	}
+function createNext() {
+	var next = document.createElement("input");
+	next.setAttribute('type', "submit");
+	next.setAttribute('value', "Next");
+	next.setAttribute('id', "next")
+	controls.appendChild(next);
 
-	createTest();
 	
 }
 
-
-
-function getAnswer() {
-	var picked;
-	for (i = 0; i < options.length; i++) {
-		if (options[i].checked) {
-			 picked = options[i].value;
-			break;
-		}
-	}
+function createPrev() {
+	var next = document.createElement("input");
+	next.setAttribute('type', "submit");
+	next.setAttribute('value', "Prev");
+	next.setAttribute('id', "prev")
+	controls.appendChild(next);
 }
 
+function displayNext() {
+	
+	
+}
 
-// document.getElementById("send").addEventListener("click", function() {
-
-
-// 	if (typeof(Storage) !== "undefined") {
-//     // Code for localStorage/sessionStorage.
-//     if (picked == correct) {
-//     	score = 1;
-//     	sessionStorage.setItem("question", "picked");
-//     } else {
-//     	score = 1;
-//     	sessionStorage.setItem("question", "picked");
-//     }
-// } else {
-//     // Sorry! No Web Storage support..
-//     alert("Your browser does not have Web Storage support");
-// }
-
-
-// }); 
-
-
-
-
+next.addEventListener("click", displayNext());
