57a47e5204cec80fd3ea0204ac062d3809486baf
Anjola <anjolabassey@yahoo.com>
2018-05-04 14:58:06 +0100

    added functionality for previous and next button

--- js/script.js
+++ js/script.js
@@ -75,35 +75,54 @@ function getTest(subject, counter) {
 			
 		document.getElementById("title").innerHTML = subject + " Test";
 		
-
-			
-				q = "<h3 id='quest'>" + questions[counter].question + "</h3>";	
-			a = "<input type='radio' id='one'>" + questions[counter].options[0] + "</input>" + "<br>";
-			b = "<input type='radio' id='two'>" + questions[counter].options[1] + "</input>" + "<br>";
-			c = "<input type='radio' id='three'>" + questions[counter].options[2] + "</input>";
-			main.innerHTML =  q + a + b + c;
-			
-			
-			
-						
+			q = "<h3 id='quest'>" + questions[counter].question + "</h3>";	
+			a = "<input type='radio' name='options' id='one' onclick='getAnswer()'>" + questions[counter].options[0] + "</input>" + "<br>";
+			b = "<input type='radio' name='options' id='two'>" + questions[counter].options[1] + "</input>" + "<br>";
+			c = "<input type='radio' name='options' id='three'>" + questions[counter].options[2] + "</input>";
+			main.innerHTML =  q + a + b + c;			
 	});
 }
+function createQuestion() {
+	var quest = document.createElement("h3");
+	var txt = document.createTextNode( questions[counter].question);
+	quest.appendChild(txt);
+	main.appendChild(quest);
+}
+function createOptions() {
+	var radio = document.createElement("input");
+	radio.setAttribute("type", "radio");
+	radio.setAttribute("name", "options");
+	
+	var ltxt = document.createTextNode(questions[counter].options[0]);
+	radio.appendChild(ltxt);
 
+	main.appendChild(radio);
+
+
+}
 function createNext() {
+	
 	var next = document.createElement("input");
 	next.setAttribute('type', "submit");
 	next.setAttribute('value', "Next");
 	next.setAttribute('id', "next")
 	controls.appendChild(next);
-
+	createPrev();
+	prev.style.visibility = "hidden";
 	
+
 	next.addEventListener("click", function() {
-		clearDiv();
+		clearDiv();	
+		
 		counter++;
 		console.log(counter);
 		console.log(questions);
+		
 		if (counter < questions.length - 1) {
 		getTest(subject, counter);
+		prev.style.visibility = "visible";
+
+		
 		
 		} else {
 			getTest(subject, counter);
@@ -121,6 +140,24 @@ function createPrev() {
 	prev.setAttribute('value', "Prev");
 	prev.setAttribute('id', "prev")
 	controls.appendChild(prev);
+
+	prev.addEventListener("click", function() {	
+		clearDiv();
+		counter--;
+		console.log(counter);
+		console.log(questions);
+		
+		if (counter > 0 && counter < questions.length -1 ) {
+		getTest(subject, counter);
+		//next.style.visibility = "visible";
+		done.remove();
+		createNext();
+		
+		} else {
+			getTest(subject, counter);
+			prev.remove();
+		}
+	});
 }
 
 function createDone() {
@@ -133,6 +170,8 @@ function createDone() {
 	done.addEventListener("click", function() {
 		main.innerHTML = "<h3>" + sessionStorage.getItem("name", name) + " with Matric No " + sessionStorage.getItem("matno", matNo) + ", your score is 0 out of " + questions.length + "</h3";
 		done.remove();
+		prev.remove();
+		
 	});
 }
 
@@ -144,5 +183,12 @@ function displayNext() {
 }
 
 function getAnswer() {
-	document.getElementById("one").value
+	//var radios = document.getElementByTagName("options");
+	var answer = document.getElementById("one").checked;
+	console.log(answer.value);
+	sessionStorage.setItem("answer", answer);
+}
+
+function timer() {
+
 }
