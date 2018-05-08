568438fea08fdd50aa8bfc0b067662be09fcc4de
Anjola <anjolabassey@yahoo.com>
2018-05-08 16:30:10 +0100

    changed script for navigation buttons

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
-var sub = document.getElementById("register");
-var subject, name, matNo; 
-
+var start = document.getElementById("start");
 var main = document.getElementById("test");
-var controls = document.getElementById("buttons");
-var counter = 0;
-var score;
+var subject, name, matNo; 
+var score = 0;
 var questions;
+var counter = 0;
+var picked;
 var i;
-var a = ""; var b = ""; var c = ""; var q = "";
 
 
-sub.addEventListener("click", function(event) {
+
+start.addEventListener("click", function(event) {
 	event.preventDefault();
 	subject = document.getElementById("subjects").value;
 	//validateForm();
 	storeUser();
 	clearDiv();
 	getTest(subject, counter);
-	createNext();
-	// displayNext();
+	// createNext();
+	//displayNext();
 });
 
 //validates the form: user cant register empty fields
@@ -54,6 +53,29 @@ function clearDiv() {
 	main.innerHTML = "";		
 }
 
+function createQuestion() {
+	var quest = document.createElement("h3");
+	quest.setAttribute("id", "quest");
+	var txt = document.createTextNode( questions[counter].question);
+	quest.appendChild(txt);
+	main.appendChild(quest);
+}
+
+function createOptions() {
+	for (var j=0; j < questions[counter].options.length; j++) {
+		
+		var radio = document.createElement("input");
+		radio.setAttribute("type", "radio");
+		radio.setAttribute("name", "options");
+		
+		var opt = document.createElement("div");
+		var ltxt = document.createTextNode( questions[counter].options[j]);
+		opt.appendChild(radio);
+		opt.appendChild(ltxt);
+		main.appendChild(opt);
+	}
+	
+}
 function getData(result) { 
 	var xhttp = new XMLHttpRequest();
 	xhttp.overrideMimeType("application/json");
@@ -68,125 +90,85 @@ function getData(result) {
 }
 
 function getTest(subject, counter) {
-	
 	getData(function (obj) {
 		result = JSON.parse(obj);
 		questions = result.subjects[subject];
-			
 		document.getElementById("title").innerHTML = subject + " Test";
-		
-			q = "<h3 id='quest'>" + questions[counter].question + "</h3>";	
-			a = "<input type='radio' name='options' id='one' onclick='getAnswer()'>" + questions[counter].options[0] + "</input>" + "<br>";
-			b = "<input type='radio' name='options' id='two'>" + questions[counter].options[1] + "</input>" + "<br>";
-			c = "<input type='radio' name='options' id='three'>" + questions[counter].options[2] + "</input>";
-			main.innerHTML =  q + a + b + c;			
+		createQuestion();
+		createOptions();
+	
+		if(counter === 0 ) {
+			next.style.display = "block";
+			
+		} else if(counter < questions.length) {
+			prev.style.display = "block";
+		} 
 	});
 }
-function createQuestion() {
-	var quest = document.createElement("h3");
-	var txt = document.createTextNode( questions[counter].question);
-	quest.appendChild(txt);
-	main.appendChild(quest);
-}
-function createOptions() {
-	var radio = document.createElement("input");
-	radio.setAttribute("type", "radio");
-	radio.setAttribute("name", "options");
+
+next.addEventListener("click", function() {
+	getAnswer();
 	
-	var ltxt = document.createTextNode(questions[counter].options[0]);
-	radio.appendChild(ltxt);
-
-	main.appendChild(radio);
-
-
-}
-function createNext() {
+	clearDiv();
+	counter++;
 	
-	var next = document.createElement("input");
-	next.setAttribute('type', "submit");
-	next.setAttribute('value', "Next");
-	next.setAttribute('id', "next")
-	controls.appendChild(next);
-	createPrev();
-	prev.style.visibility = "hidden";
-	
-
-	next.addEventListener("click", function() {
-		clearDiv();	
-		
-		counter++;
-		console.log(counter);
-		console.log(questions);
-		
-		if (counter < questions.length - 1) {
+	if (counter < questions.length - 1) {
 		getTest(subject, counter);
 		prev.style.visibility = "visible";
-
 		
-		
-		} else {
-			getTest(subject, counter);
-			next.remove();
-			createDone();
-		}
-		
-	});
-	
-}
-
-function createPrev() {
-	var prev = document.createElement("input");
-	prev.setAttribute('type', "submit");
-	prev.setAttribute('value', "Prev");
-	prev.setAttribute('id', "prev")
-	controls.appendChild(prev);
-
-	prev.addEventListener("click", function() {	
-		clearDiv();
-		counter--;
-		console.log(counter);
-		console.log(questions);
-		
-		if (counter > 0 && counter < questions.length -1 ) {
+	} else {
 		getTest(subject, counter);
-		//next.style.visibility = "visible";
-		done.remove();
-		createNext();
+		next.style.display = "none";
+		prev.style.display = "none";
+		done.style.display = "block";
+
+	}
+});
+
+
+prev.addEventListener("click", function() {	
+	clearDiv();
+	counter--;
 		
-		} else {
-			getTest(subject, counter);
-			prev.remove();
-		}
-	});
-}
+	if (counter === 0 ) {
+		getTest(subject, counter);
+		prev.style.display = "none";
+		done.style.display = "none";
+	} else {
+		getTest(subject, counter);
+			
+	}
+});
 
-function createDone() {
-	var done = document.createElement("input");
-	done.setAttribute('type', "submit");
-	done.setAttribute('value', "Done");
-	done.setAttribute('id', "done")
-	controls.appendChild(done);
+done.addEventListener("click", function() {
+	getAnswer();	
+	displayScore();
+	again.style.display = "inline";
+	prev.style.display = "none";
+	done.style.display = "none";
+});
 	
-	done.addEventListener("click", function() {
-		main.innerHTML = "<h3>" + sessionStorage.getItem("name", name) + " with Matric No " + sessionStorage.getItem("matno", matNo) + ", your score is 0 out of " + questions.length + "</h3";
-		done.remove();
-		prev.remove();
+again.addEventListener("click", function() {
+	clearDiv();
+	counter = 0;
+	getTest(subject, counter);
+	again.style.display = "none";		
+});
+
+function displayScore() {
+	for (var j=0; j < questions.length; j++) {
+		if(picked == questions[counter].answer[j]) {
+			score++;
+		} 
 		
-	});
+	}
+	main.innerHTML = "<h3>" + sessionStorage.getItem("name", name) + " with Matric No " + sessionStorage.getItem("matno", matNo) + ", your score is " + score + " out of " + questions.length + "</h3";
 }
-
-
-
-function displayNext() {
-	
-	
-}
-
 function getAnswer() {
-	//var radios = document.getElementByTagName("options");
-	var answer = document.getElementById("one").checked;
-	console.log(answer.value);
-	sessionStorage.setItem("answer", answer);
+	questions[counter] = document.querySelector("r").checked.value;
+	picked = questions[counter];
+	console.log(picked);
+	// sessionStorage.setItem("answer", answer);
 }
 
 function timer() {
